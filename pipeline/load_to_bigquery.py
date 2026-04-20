0065051010fc5beb4f7b4d647e58b8172126871e
Jorge Carhuarica <lexbetaOS@gmail.com>
2026-04-20 16:53:33 -0500

    Refactor load_to_bigquery to accept table and URI args

--- pipeline/load_to_bigquery.py
+++ pipeline/load_to_bigquery.py
@@ -1,42 +1,42 @@
 from google.cloud import bigquery
+import os
+import argparse
 
-# CONFIG
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/workspace/keys/gcp-key.json"
+
 PROJECT_ID = "kestra-sandbox-487804"
 DATASET_ID = "flights_dataset"
-# TABLE_ID = "fct_flight_delays"
-TABLE_ID = "dim_delay_causes"
-# GCS_URI = "gs://de-zoomcamp-flightsdelay-data/flights/mart/*.csv"
-GCS_URI = "gs://de-zoomcamp-flightsdelay-data/flights/mart/dim_delay_causes_gcp_stage/*.csv"
 
-def main():
+
+def load_table(table_id, gcs_uri):
     client = bigquery.Client(project=PROJECT_ID)
 
-    table_ref = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
+    table_ref = f"{PROJECT_ID}.{DATASET_ID}.{table_id}"
 
     job_config = bigquery.LoadJobConfig(
         source_format=bigquery.SourceFormat.CSV,
         skip_leading_rows=1,
         autodetect=True,
         write_disposition="WRITE_TRUNCATE",
-
-        time_partitioning=bigquery.TimePartitioning(
-            field="YEAR"   # or a date field if you have it
-        ),
-
-        clustering_fields=["AIRPORT", "CARRIER_NAME"]
     )
 
-    print(f"Loading data into {table_ref}...")
+    print(f"Loading {table_id} from {gcs_uri}...")
 
     load_job = client.load_table_from_uri(
-        GCS_URI,
+        gcs_uri,
         table_ref,
         job_config=job_config,
     )
 
-    load_job.result()  # Wait for job
+    load_job.result()
+    print(f"✅ {table_id} loaded!")
 
-    print("✅ Load completed!")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--table", required=True)
+    parser.add_argument("--uri", required=True)
+
+    args = parser.parse_args()
+
+    load_table(args.table, args.uri)
